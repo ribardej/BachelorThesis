39684c0e580e2022da3c8c8ea9c2d4103d96b208
ribardej <ribardej@student.cvut.cz>
2024-04-25 14:09:57 +0200

    better version

--- cpu/cpuBenchmark.sh
+++ cpu/cpuBenchmark.sh
@@ -8,21 +8,13 @@ run() {
     echo "$runtimeName: $elapsedTime"
 }
 
-runMatrix() {
-    local runtimeName=$1
-    local runCommand=$2
-
-    local output=$( { time $runCommand; } 2>&1 | head -n 1)
-    echo "$runtimeName: $output iterations made"
-}
-
 echo -e "\n1) Recursive computation of 43rd fibonacci number"
 run "Node" "node fibonacci.js"
 run "Deno" "deno run fibonacci.js"
 run "Bun" "bun fibonacci.js"
 
 echo -e "\n2) 10x10 matrix multiplication for 1s"
-runMatrix "Node" "node matrixMult.js"
-runMatrix "Deno" "deno run matrixMult.js"
-runMatrix "Bun" "bun matrixMult.js"
+echo "Node: $(node matrixMult.js) iterations made"
+echo "Deno: $(deno run matrixMult.js) iterations made"
+echo "Bun: $(bun matrixMult.js) iterations made"
 echo ""
