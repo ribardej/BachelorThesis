beaae8c3cc65f7b8da0dcab87aad64fcd95b14a0
ribardej <ribardej@student.cvut.cz>
2024-04-26 15:06:38 +0200

    better version

--- cpu/matrixMult.js
+++ cpu/matrixMult.js
@@ -1,19 +1,4 @@
-function matrixMultiplication(a, b) {
-    const result = [];
-    for (let i = 0; i < a.length; i++) {
-        result[i] = [];
-        for (let j = 0; j < b[0].length; j++) {
-        let sum = 0;
-            for (let k = 0; k < a[0].length; k++) {
-                sum += a[i][k] * b[k][j];
-            }
-        result[i][j] = sum;
-        }
-    }
-    return result;
-}
-
-const matrixA = [
+let matrixA = [
     [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     [10, 1, 2, 3, 4, 5, 6, 7, 8, 9],
     [9, 10, 1, 2, 3, 4, 5, 6, 7, 8],
@@ -25,6 +10,22 @@ const matrixA = [
     [3, 4, 5, 6, 7, 8, 9, 10, 1, 2],
     [2, 3, 4, 5, 6, 7, 8, 9, 10, 1]
 ];
+
+function matrixMultiplication(a, b) {
+    const result = [];
+    for (let i = 0; i < a.length; i++) {
+        result[i] = [];
+        for (let j = 0; j < b[0].length; j++) {
+            let sum = 0;
+            for (let k = 0; k < a[0].length; k++) {
+                sum += a[i][k] * b[k][j];
+            }
+            result[i][j] = sum;
+        }
+    }
+    return result;
+}
+
 
 function cpuBenchmark() {
     const start = Date.now();
